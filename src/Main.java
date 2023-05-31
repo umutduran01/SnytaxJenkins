95bfc9a9bc82fe2817528f8917890869bc0a41e0
Umut Duran <umutduran.95@hotmail.com>
2023-05-31 17:13:32 +0100

    Time change

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello world!");
-            Thread.sleep(2000);
+            Thread.sleep(3000);
         }
     }
 }
